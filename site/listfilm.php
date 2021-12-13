2ccb9b12bd1f11363ef47db057e3b0ba1508c38f
Arnaud LOUISY <LOUISY_Ar@lprs.local>
2021-12-13 15:49:07 +0100

    modification de la page liste de films

--- site/listfilm.php
+++ site/listfilm.php
@@ -16,19 +16,28 @@ $res=$req -> fetchAll();
     <title></title>
   </head>
   <body>
-  <p>Liste des films</p>
   <table>
+      <center>
 
+      <p><h1>Liste des films</h1></p>
+      </center>
+      <br>
+      <br>
+      <br>
 
 <?php
 foreach ($res as $value){
+
+?>
+    <?php
     echo $value ['titre']."<br>";
     echo $value ['description']."<br>";
-    echo $value ['anne_sortie']."<br>";
-    echo $value ['description']."<br>";
-     echo $value ['image']."<br>"."<br>";
+    echo $value ['annee_sortie']."<br>";
+    ?>
+      <img src= <?php  echo $value ['image']; ?>  alt="" width="200">
+      <?php
 }
-?>
+      ?>
   </table>
   </body>
 </html>
